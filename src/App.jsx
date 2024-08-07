b27ec47731b4dc17c3fdd7d7cf8044d818ed2aa2
WeCanStart <artemhapilov2004@gmail.com>
2024-08-07 15:49:05 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+export class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    
+    };
+  }
+
+  render() {
+    return (
+      null
+    );
+  }
+}
