c3f3525509d8728e92f89b06eea97189ce7d1b73
WeCanStart <artemhapilov2004@gmail.com>
2024-08-08 00:00:30 +0300

    basic interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+
+import { SightPrinter } from './SightPrinter.jsx';
 
 export class App extends React.Component {
 
@@ -8,13 +12,55 @@ export class App extends React.Component {
     super(props);
 
     this.state = {
-    
+      city: "null",
+      sights: {
+        "geo": [],
+        "msk": [],
+        "spb": [],
+        "nsk": [],
+        "ekb": [],
+        "kzn": [],
+        "nn": []
+      }
     };
+  }
+
+  handleChange = (event) => {
+    this.setState({ city: event?.target?.value });
   }
 
   render() {
     return (
-      null
+      <Router>
+        <Routes>
+          <Route path="/" element = {
+            <div>
+              <select name="location" value={this.state.city} onChange={this.handleChange}>
+                <option default value="null">Выберите город</option>
+                <option value="geo">Геолокация</option>
+                <option value="msk">Москва</option>
+                <option value="spb">Санкт-Петербург</option>
+                <option value="nsk">Новосибирск</option>
+                <option value="ekb">Екатеринбург</option>
+                <option value="kzn">Казань</option>
+                <option value="nn">Нижний Новгород</option>
+              </select>
+              <SightPrinter city={this.state.city}/>
+            </div>
+          }/>
+            
+          <Route path="/login" element = {
+            <div>
+              <form className="login" /*</div>onSubmit={this.handleSubmit}*/ >
+                <input type="text" name="email" placeholder="email" />
+                <input type="password" name="password" placeholder="password" />
+                <button name="login" /*onClick={this.handleLogin}*/>Войти</button>
+              </form>
+              <button name="register" /*onClick={this.handleRegister}*/>Зарегистрироваться</button>
+            </div>
+          }/>
+        </Routes>
+      </Router>
     );
   }
 }
